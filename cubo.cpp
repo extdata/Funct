3ff22d1d1fa520ba2d28b8f05720cee4f003d22c
Ricardo <rjmaciel90@gmail.com>
2017-11-01 20:04:17 -0300

    Continuação da aula de função

--- cubo.cpp
+++ cubo.cpp
@@ -2,10 +2,14 @@
 using std::cin;
 using std::cout;
 
-int cubo (int a)
-{
-	return a*a*a;
-}
+int cuboPorValor ( int );
+void cuboPorRef ( int & );
+int calc_volume(int, int, int);
+double calc_volume(double = 2.0 , double = 2.0, double = 2.0);
+
+
+
+
 
 int main()
 {
@@ -14,7 +18,72 @@ int main()
 	cout << "\nDigite um valor para calcular o cubo: ";
 	cin >> x;
 	
-	cout << "\nO resultado " << char (130) << ": " << cubo (x) << '\n';
+	cout << "\nO resultado eh: " << cuboPorValor (x) << '\n';
 	
-	return 0;
+    
+    int y;
+    
+    cout << "\nDigite um valor para calcular o cubo: ";
+    cin >> y;
+    
+    
+    cuboPorRef(y);
+    
+    cout << "\nO resultado eh: " << y << '\n';
+    
+    
+    int comp, larg, alt;
+    
+    cout << "\nDigite o valor do comprimento: ";
+    cin >> comp;
+    
+    cout << "\nDigite o valor da largura: ";
+    cin >> larg; 
+    
+    cout << "\nDigite o valor da altura: ";
+    cin >> alt;
+    
+    cout << "\nVolume eh: " << calc_volume(comp, larg, alt);
+    
+    
+    double comp_, larg_, alt_;
+    
+    cout << "\nDigite o valor do comprimento: ";
+    cin >> comp_;
+    
+    cout << "\nDigite o valor da largura: ";
+    cin >> larg_; 
+    
+    cout << "\nDigite o valor da altura: ";
+    cin >> alt_;
+    
+    cout << "\nVolume eh: " << calc_volume(comp_, larg_, alt_);
+    
+    
+    cout << "\nVolume eh: " << calc_volume();
+    
+    cout << "\nVolume eh: " << calc_volume(comp_);
+    
+    
+    
+    return 0;
+}
+
+int cuboPorValor(int a)
+{
+	return a*a*a;
+}
+
+void cuboPorRef( int & a )
+{
+    a *= a * a;
+}
+
+int calc_volume(int comp, int larg, int alt)
+{
+    return comp * larg * alt;
+}
+double calc_volume(double comp_, double larg_, double alt_)
+{
+    return comp_ * larg_ * alt_;
 }
